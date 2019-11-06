d94f3cb5465190a8ea40f9a97a4c0439c47b3c8a
Remy <remyj.duval@gmail.com>
2019-11-06 17:03:48 +0100

    initial commit bis

--- app/scripts/App.js
+++ app/scripts/App.js
@@ -1,57 +1,64 @@
+import * as THREE from "three"
 export default class App {
+    constructor() {
+        this.container = document.querySelector("#main")
+        document.body.appendChild(this.container)
 
-	constructor() {
+        this.camera = new THREE.PerspectiveCamera(
+            45,
+            window.innerWidth / window.innerHeight,
+            1,
+            1000
+        )
+        this.camera.position.z = 10
 
-		this.container = document.querySelector('#main');
-		document.body.appendChild(this.container);
+        this.scene = new THREE.Scene()
 
-		this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-		this.camera.position.z = 10;
+        this.renderer = new THREE.WebGLRenderer({ antialias: true })
+        this.renderer.setPixelRatio(window.devicePixelRatio)
+        this.renderer.setSize(window.innerWidth, window.innerHeight)
+        this.container.appendChild(this.renderer.domElement)
 
-		this.scene = new THREE.Scene();
+        window.addEventListener("resize", this.onWindowResize.bind(this), false)
+        this.onWindowResize()
 
-		this.renderer = new THREE.WebGLRenderer({antialias: true});
-		this.renderer.setPixelRatio(window.devicePixelRatio);
-		this.renderer.setSize(window.innerWidth, window.innerHeight);
-		this.container.appendChild(this.renderer.domElement);
+        this.renderer.animate(this.render.bind(this))
 
-		window.addEventListener('resize', this.onWindowResize.bind(this), false);
-		this.onWindowResize();
+        let cube = new Cube().mesh
 
-		this.renderer.animate(this.render.bind(this));
+        this.scene.add(cube)
 
-		let cube = new Cube().mesh;
+        this.render()
+    }
 
-		this.scene.add(cube);
+    render() {
+        this.renderer.render(this.scene, this.camera)
+    }
 
-		this.render();
-	}
-
-	render() {
-		this.renderer.render(this.scene, this.camera);
-	}
-
-	onWindowResize() {
-
-		this.camera.aspect = window.innerWidth / window.innerHeight;
-		this.camera.updateProjectionMatrix();
-		this.renderer.setSize(window.innerWidth, window.innerHeight);
-	}
+    onWindowResize() {
+        this.camera.aspect = window.innerWidth / window.innerHeight
+        this.camera.updateProjectionMatrix()
+        this.renderer.setSize(window.innerWidth, window.innerHeight)
+    }
 }
 
 class Cube {
-	constructor(width = 1, height = 1, depth = 1, color = 0xFFFFFF) {
-		this.width = width;
-		this.height = height;
-		this.depth = depth;
-		this.color = color;
+    constructor(width = 1, height = 1, depth = 1, color = 0xffffff) {
+        this.width = width
+        this.height = height
+        this.depth = depth
+        this.color = color
 
-		this.mesh = this.createCube();
-	}
+        this.mesh = this.createCube()
+    }
 
-	createCube() {
-		let geometry = new THREE.BoxBufferGeometry( this.width, this.height, this.depth);
-		let material = new THREE.MeshBasicMaterial( { color: this.color } );
-		return new THREE.Mesh(geometry, material);
-	}
+    createCube() {
+        let geometry = new THREE.BoxBufferGeometry(
+            this.width,
+            this.height,
+            this.depth
+        )
+        let material = new THREE.MeshBasicMaterial({ color: this.color })
+        return new THREE.Mesh(geometry, material)
+    }
 }
