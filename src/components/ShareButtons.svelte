102e4de5a9898a1c014723970ea9f5019fce8aee
Mael <atzorimaltete.mael@gmail.com>
2019-12-03 17:56:00 +0100

    adding interview for intro

---
+++ src/components/ShareButtons.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { fade } from "svelte/transition"
+
+    import delayBeforeNextStep from "../js/utils/delayBeforeNextStep"
+
+    export let text
+    export let subText = ""
+    export let duration
+
+    delayBeforeNextStep(duration)
+</script>
+
+<style>
+    .picto-container {
+        height: 8rem;
+        margin: auto;
+        margin-bottom: 2rem;
+    }
+    .picto-container :global(svg) {
+        height: 100%;
+    }
+</style>
+
+<div transition:fade class="center">
+    <div class="picto-container">
+        <slot />
+    </div>
+    <h3>
+        {@html text}
+    </h3>
+    {#if subText}
+        <h4>
+            {@html subText}
+        </h4>
+    {/if}
+</div>
